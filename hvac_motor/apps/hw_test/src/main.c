33fe4fba513fc35b897187f37a1d7c639fb6aab3
Sidharth Prabukumar <sidharth.prabukumar@gmail.com>
2023-05-28 17:39:53 -0400

    PWM Test

--- hvac_motor/apps/hw_test/src/main.c
+++ hvac_motor/apps/hw_test/src/main.c
@@ -1,116 +1,49 @@
-/*
- * Copyright (c) 2022 Libre Solar Technologies GmbH
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
-#include <zephyr/drivers/uart.h>
+#include <zephyr/drivers/pwm.h>
 
-#include <string.h>
+#define DT_ALIAS_PWM_0_LABEL "PWM_1"
 
-/* change this to any other UART peripheral if desired */
-#define UART_DEVICE_NODE DT_CHOSEN(zephyr_shell_uart)
+/* period of motor signal ->  20ms (50Hz) */
+#define PERIOD (NSEC_PER_SEC / 50U)
 
-#define MSG_SIZE 32
+#define SLEEP_TIME_MS 500  /* Pause time in seconds*/
 
-/* queue to store up to 10 messages (aligned to 4-byte boundary) */
-K_MSGQ_DEFINE(uart_msgq, MSG_SIZE, 10, 4);
-
-static const struct device *const uart_dev = DEVICE_DT_GET(UART_DEVICE_NODE);
-
-/* receive buffer used in UART ISR callback */
-static char rx_buf[MSG_SIZE];
-static size_t rx_buf_pos;
-
-/*
- * Read characters from UART until line end is detected. Afterwards push the
- * data to the message queue.
- */
-void serial_cb(__attribute__((unused))const struct device *dev, __attribute__((unused))void *user_data)
+void main(void)
 {
-	uint8_t c;
+	printk("HVAC Motor Control - PWM Test\n");
 
-	if (!uart_irq_update(uart_dev)) {
-		return;
-	}
+    const struct device *pwm_dev;
 
-	if (!uart_irq_rx_ready(uart_dev)) {
-		return;
-	}
-
-	/* read until FIFO empty */
-	while (uart_fifo_read(uart_dev, &c, 1) == 1) {
-		if ((c == '\n' || c == '\r') && rx_buf_pos > 0) {
-			/* terminate string */
-			rx_buf[rx_buf_pos] = '\0';
-
-			/* if queue is full, message is silently dropped */
-			k_msgq_put(&uart_msgq, &rx_buf, K_NO_WAIT);
-
-			/* reset the buffer (it was copied to the msgq) */
-			rx_buf_pos = 0;
-		} else if (rx_buf_pos < (sizeof(rx_buf) - 1)) {
-			rx_buf[rx_buf_pos++] = c;
-		}
-		/* else: characters beyond buffer size are dropped */
-	}
-}
-
-/*
- * Print a null-terminated string character by character to the UART interface
- */
-void print_uart(char *buf)
-{
-	int msg_len = strlen(buf);
-
-	for (int i = 0; i < msg_len; i++) {
-		uart_poll_out(uart_dev, buf[i]);
-	}
-}
-
-int main(void)
-{
-	char tx_buf[MSG_SIZE];
-
-	if (!device_is_ready(uart_dev)) {
-		printk("UART device not found!");
-		return 0;
-	}
-	struct uart_config uart_cfg = {0};
-	if(uart_config_get(uart_dev, &uart_cfg) != 0)
+	pwm_dev = device_get_binding(DT_ALIAS_PWM_0_LABEL);
+	if (!pwm_dev)
 	{
-		printk("Failed to get UART config");
-		return -EINVAL;
+		printk("Cannot find PWM device!\n");
+		return;
 	}
-	printk("UART_CONFIG = %d, %d, %d, %d, %d\r\n", uart_cfg.baudrate,
-	uart_cfg.parity, uart_cfg.stop_bits, uart_cfg.data_bits,
-	uart_cfg.flow_ctrl);
-
-	/* configure interrupt and callback to receive data */
-	int ret = uart_irq_callback_user_data_set(uart_dev, serial_cb, NULL);
-
-	if (ret < 0) {
-		if (ret == -ENOTSUP) {
-			printk("Interrupt-driven UART API support not enabled\n");
-		} else if (ret == -ENOSYS) {
-			printk("UART device does not support interrupt-driven API\n");
-		} else {
-			printk("Error setting UART callback: %d\n", ret);
-		}
-		return 0;
+	else
+	{
+		printk("PWM device find\n");
+		printk("%s\n", DT_ALIAS_PWM_0_LABEL);
 	}
-	uart_irq_rx_enable(uart_dev);
-
-	print_uart("Hello! I'm your echo bot.\r\n");
-	print_uart("Tell me something and press enter:\r\n");
-
-	/* indefinitely wait for input from the user */
-	while (k_msgq_get(&uart_msgq, &tx_buf, K_FOREVER) == 0) {
-		print_uart("Echo: ");
-		print_uart(tx_buf);
-		print_uart("\r\n");
+	printk("PWM device cycle 1\n");
+	if (pwm_set(pwm_dev, 1, PERIOD, (PERIOD * 0.9), 0))
+	{
+		printk("PWM pin set fails\n");
+		return;
 	}
-	return 0;
+	printk("PWM device cycle 2\n");
+	if (pwm_set(pwm_dev, 2, PERIOD, (PERIOD * 0.5), 0))
+	{
+		printk("PWM pin set fails\n");
+		return;
+	}
+	printk("PWM device cycle 3\n");
+	if (pwm_set(pwm_dev, 3, PERIOD, (PERIOD * 0.3), 0))
+	{
+		printk("PWM pin set fails\n");
+		return;
+	}
+    printk("Running...\n");
+
 }
